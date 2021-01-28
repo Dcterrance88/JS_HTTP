02a8d6dd1a618d8a86d61be94292272a90b823a0
Dcterrance88 <>
2021-01-27 20:01:11 -0500

    CRUD - Borrar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import * as CRUD from './js/crud-provider';
 
 
-CRUD.getUsuario(2).then(console.log);
+// CRUD.getUsuario(2).then(console.log);
 
+// CRUD.crearUsuario({
+//     name: 'Froilan',
+//     job: 'Programador'
+// }).then(console.log);
 
-CRUD.crearUsuario({
-    name: 'Froilan',
-    job: 'Programador'
-}).then(console.log);
+// CRUD.actualizarUsuario( 1, {
+//     name: 'Melissa',
+//     job:'Senadora'
+// }).then(console.log);
 
-CRUD.actualizarUsuario( 1, {
-    name: 'Melissa',
-    job:'Senadora'
-}).then(console.log);
+CRUD.borrarUsuario(1).then(console.log);
